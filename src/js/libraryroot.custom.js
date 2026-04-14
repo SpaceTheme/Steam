8da6168ea2107e8da6b9f1ecead2ed3adc021c10
S0rax <s0rax@dmginc.gg>
2026-04-15 01:40:48 +0200

    feat: add fancy hover on game title

--- src/js/libraryroot.custom.js
+++ src/js/libraryroot.custom.js
@@ -55,63 +55,137 @@ waitForElement('.Rp8QOGJ2DypeDniMnRBhr').then(() => {
 
 // Store Sidebar Width half fix
 async function syncWidthIfTargetHidden() {
-  const sourceClass = '._9sPoVBFyE_vE87mnZJ5aB';
-  const targetClass = '.RGNMWtyj73_-WdhflrmuY';
+    const sourceClass = '._9sPoVBFyE_vE87mnZJ5aB';
+    const targetClass = '.RGNMWtyj73_-WdhflrmuY';
 
-  const sourceEl = await waitForElement(sourceClass);
-  const targetEl = await waitForElement(targetClass);
+    const sourceEl = await waitForElement(sourceClass);
+    const targetEl = await waitForElement(targetClass);
 
-  const setWidth = () => {
-    const width = sourceEl.style.width;
-    if (width) {
-      targetEl.style.width = width;
-    }
-  };
-
-  const removeWidth = () => {
-    targetEl.style.removeProperty('width');
-  };
-
-  const handleTargetDisplayChange = () => {
-    if (targetEl.style.display === 'none') {
-      setWidth();
-    } else {
-      removeWidth();
-    }
-  };
-
-  // Initial check
-  handleTargetDisplayChange();
-
-  // Watch for style changes on source
-  const sourceObserver = new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-      if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-        if (targetEl.style.display === 'none') {
-          setWidth();
+    const setWidth = () => {
+        const width = sourceEl.style.width;
+        if (width) {
+            targetEl.style.width = width;
         }
-      }
-    }
-  });
+    };
 
-  sourceObserver.observe(sourceEl, {
-    attributes: true,
-    attributeFilter: ['style'],
-  });
+    const removeWidth = () => {
+        targetEl.style.removeProperty('width');
+    };
 
-  // Watch for style changes on target (especially display)
-  const targetObserver = new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-      if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-        handleTargetDisplayChange();
-      }
-    }
-  });
+    const handleTargetDisplayChange = () => {
+        if (targetEl.style.display === 'none') {
+            setWidth();
+        } else {
+            removeWidth();
+        }
+    };
 
-  targetObserver.observe(targetEl, {
-    attributes: true,
-    attributeFilter: ['style'],
-  });
+    // Initial check
+    handleTargetDisplayChange();
+
+    // Watch for style changes on source
+    const sourceObserver = new MutationObserver((mutations) => {
+        for (const mutation of mutations) {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+                if (targetEl.style.display === 'none') {
+                    setWidth();
+                }
+            }
+        }
+    });
+
+    sourceObserver.observe(sourceEl, {
+        attributes: true,
+        attributeFilter: ['style'],
+    });
+
+    // Watch for style changes on target (especially display)
+    const targetObserver = new MutationObserver((mutations) => {
+        for (const mutation of mutations) {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+                handleTargetDisplayChange();
+            }
+        }
+    });
+
+    targetObserver.observe(targetEl, {
+        attributes: true,
+        attributeFilter: ['style'],
+    });
 }
 
 syncWidthIfTargetHidden();
+
+(async function setupGamesHovers() {
+    const gamesContainer = await waitForElement('._1ijTaXJJA5YWl_fW2IxcaT .ReactVirtualized__Grid__innerScrollContainer');
+    const itemSelector = '._2-O4ZG0KrnSrzISHBKctFQ';
+    const separatorSelector = '._2RggXvVkWMDvvxFegjtKso';
+    const widthContainerSelector = '._2SXJM0PeFEi3gbC7V3S5pE';
+    const pxPerSec = 70;
+    const minDurationSec = 2;
+
+    const setHoverVariables = (item, overflow) => {
+        if (overflow <= 0) {
+            item.style.removeProperty('--st-game-overflow');
+            item.style.removeProperty('--st-game-hover-duration');
+            return;
+        }
+
+        const duration = Math.max(minDurationSec, overflow / pxPerSec);
+        item.style.setProperty('--st-game-overflow', `${overflow}px`);
+        item.style.setProperty('--st-game-hover-duration', `${duration}s`);
+    };
+
+    const getOverflowWidth = (item, itemText, separator) => {
+        if (!separator) {
+            return itemText.scrollWidth - itemText.clientWidth + 5;
+        }
+
+        itemText.style.setProperty('padding-right', `${separator.offsetWidth + 4}px`);
+        const widthContainer = item.querySelector(widthContainerSelector);
+        if (!widthContainer) {
+            return itemText.scrollWidth - itemText.clientWidth + 5;
+        }
+
+        return widthContainer.scrollWidth - widthContainer.clientWidth;
+    };
+
+    const handleGamesHover = () => {
+        const gameItems = gamesContainer.querySelectorAll(itemSelector);
+
+        gameItems.forEach(item => {
+            const itemText = item.querySelector('span');
+            if (!itemText) return;
+
+            const separator = itemText.querySelector(separatorSelector);
+
+            if (!itemText.dataset.stHoverText) {
+                let text = itemText.innerText;
+                if (separator) text += ` ${separator.textContent}`;
+                itemText.dataset.stHoverText = text;
+            }
+
+            const widthDiff = getOverflowWidth(item, itemText, separator);
+            const overflow = Math.max(0, Math.ceil(widthDiff));
+            setHoverVariables(item, overflow);
+        });
+    };
+
+    let rafId = 0;
+    const scheduleHoverUpdate = () => {
+        if (!rafId) {
+            rafId = requestAnimationFrame(() => {
+                rafId = 0;
+                handleGamesHover();
+            });
+        }
+    };
+
+    const attrObserver = new MutationObserver(scheduleHoverUpdate);
+    attrObserver.observe(gamesContainer, { attributes: true, attributeFilter: ['style'] });
+
+    const childObserver = new MutationObserver(scheduleHoverUpdate);
+    childObserver.observe(gamesContainer, { childList: true, subtree: true });
+
+    handleGamesHover();
+})();
