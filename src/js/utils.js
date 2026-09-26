d62c0d780a4120e4c6c3102a0202b9c1f9c723f4
S0rax <s0rax@dmginc.gg>
2026-09-26 22:18:03 +0200

    fix panel resize on right side

--- src/js/utils.js
+++ src/js/utils.js
@@ -55,3 +55,26 @@ export const createContainerTracker = (selector, { onCleanup, onSetup, onUpdate 
         setContainer: (newContainer) => { container = newContainer; },
     };
 };
+
+export const CssClassNames = {
+    Root: '.Rp8QOGJ2DypeDniMnRBhr',
+    MainBody: '.RGNMWtyj73_-WdhflrmuY',
+    Library: {
+        Body: '._9sPoVBFyE_vE87mnZJ5aB',
+        Container: '._3x1HklzyDs4TEjACrRO2tB',
+        Divider: '._276E6ijBpjMA2_iTxNhhjc',
+        Games: {
+            Container: '._1ijTaXJJA5YWl_fW2IxcaT',
+            Game: '._2-O4ZG0KrnSrzISHBKctFQ',
+            Separator: '._2RggXvVkWMDvvxFegjtKso',
+            Name: '._2SXJM0PeFEi3gbC7V3S5pE',
+        }
+    },
+    UserPanel: {
+        Container: '._3cykd-VfN_xBxf3Qxriccm',
+        Button: '._2Szzh5sKyGgnLUR870zbDE',
+        FriendButton: '._1TdaAqMFadi0UTqilrkelR',
+        FamilyButton: '._13vrqU6oOqmmxrsZSW5O39',
+    },
+    DownloadBar: '._1_yS5UP7el0aN4vntx3dx',
+};
